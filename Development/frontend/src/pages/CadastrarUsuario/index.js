8d1ffeab54d5a4271a3e38cd4091cc3b691431e8
unknown <kevin.bernardo64@gmail.com>
2020-10-16 16:58:48 -0300

    conectado login

--- Development/frontend/src/pages/CadastrarUsuario/index.js
+++ Development/frontend/src/pages/CadastrarUsuario/index.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import cadastrar from '../../images/cadastrar.svg';
 
 import { Container, Left, Rigth, Login, Content, InputBox, InputWrapper, ButtonBox } from './styles';
 
+
+import ApiCadastro from '../../services/Cadastro/services';
+
+const api = new ApiCadastro();
+
 function CadastrarUsuario() {
+
+    const [nomeCompleto , setNomeCompleto] = useState('');
+    const [nomeUsuario, setNomeUsuario] = useState('');
+    const [email , setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const cadastrarUsuario = async () =>{
+        try {
+
+            const resp = await api.cadastroCliente({
+                nomeCompleto,
+                nomeUsuario,
+                email,
+                senha
+            })
+
+            return resp;
+        } catch (e) {
+            console.log(e.response.erro)
+        }
+    }
+
   return (
     <Container>
 
@@ -28,23 +55,28 @@ function CadastrarUsuario() {
 
                     <InputWrapper>
                         <span>Nome Completo</span>
-                        <input type="text" placeholder="Bruce Wayne da Silva" />
+                        <input type="text" placeholder="Bruce Wayne da Silva" onChange={e => setNomeCompleto(e.target.value)}/>
+                    </InputWrapper>
+
+                    <InputWrapper>
+                        <span>Nome de Usuário</span>
+                        <input type="text" placeholder="Batman" onChange={e => setNomeUsuario(e.target.value)}/>
                     </InputWrapper>
 
                     <InputWrapper>
                         <span>Email</span>
-                        <input type="email" placeholder="Bruce.Wayne@gmail.com" />
+                        <input type="email" placeholder="Bruce.Wayne@gmail.com" onChange={e => setEmail(e.target.value)} />
                     </InputWrapper>
 
                     <InputWrapper>
                         <span>Senha</span>
-                        <input type="password" placeholder="Pelo menos 8 caracteres" />
+                        <input type="password" placeholder="Pelo menos 8 caracteres" onChange={e => setSenha(e.target.value)}/>
                     </InputWrapper>
 
                 </InputBox>
 
                 <ButtonBox>
-                    <button>Criar minha conta grátis</button>
+                    <button onClick={cadastrarUsuario}>Criar minha conta!</button>
                     <Link to="/">Cancelar</Link>
                 </ButtonBox>
 
---
+++ Development/frontend/src/services/Cadastro/services.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const apiCadastro = axios.create({
+    baseURL: 'http://localhost:5000/usuario' 
+});
+
+export default class ApiCadastro{
+
+    async cadastroCliente(req){
+        const res = await apiCadastro.post('/cadastrar', req);
+        return res.data;
+    }
+
+}
