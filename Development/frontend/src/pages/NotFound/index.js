ee7c6c6dae1794daafbf08b35fcbbf23d0855dbc
uArthurSPereira <arthursp7007@gmail.com>
2020-10-19 16:20:36 -0200

    Not Found Done

---
+++ Development/frontend/src/pages/NotFound/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles.css';
+
+function NotFound() {
+  return (
+      <div className="not-found">
+      </div>
+  );
+}
+
+export default NotFound;
--- Development/frontend/src/routes.js
+++ Development/frontend/src/routes.js
@@ -12,16 +12,19 @@ import Login from './pages/Login';
 import EsqueciSenha from './pages/Login/EsqueciSenha';
 import AutenticacaoSenha from './pages/Login/AutenticacaoSenha';
 
+//Not Found
+import NotFound from './pages/NotFound';
+
 function Routes() {
   return(
     <BrowserRouter>
         <Switch>
             <Route path="/" component={Inicial} exact/>
             <Route path="/Criar-uma-conta" component={CadastrarUsuario}/>
-
             <Route path="/Entrar" component={Login} />
             <Route path="/Esqueci-a-senha" component={EsqueciSenha} exact/>
             <Route path="/Esqueci-a-senha/Autenticacao" component={AutenticacaoSenha} /> 
+            <Route path="*" component={NotFound} />
         </Switch>
     </BrowserRouter>
   );
