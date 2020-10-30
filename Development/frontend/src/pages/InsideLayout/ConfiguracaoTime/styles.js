5ee4eb7697d87cea172220ab6111dc4fdb4a3e58
unknown <kevin.bernardo64@gmail.com>
2020-10-30 15:39:40 -0300

    Style Config time -- done

--- Development/frontend/src/pages/InsideLayout/ConfiguracaoTime/styles.js
+++ Development/frontend/src/pages/InsideLayout/ConfiguracaoTime/styles.js
@@ -6,20 +6,79 @@ export const Container = styled.div`
 
   display:flex;
   flex-direction:column;
+
+  padding:20px 35px;
 `;
 
 export const Content = styled.div`
+  display:flex;
+  flex-direction:column;
 
+  margin:20px 0;
+
+  overflow:scroll;
+  &::-webkit-scrollbar{
+      display:none;
+  }
 `;
 
 export const InputWrapper = styled.div`
+  display:flex;
 
+  margin:15px 0;
+
+  > span{
+      margin-right:30px;
+  }
+
+  > input{
+      width:400px;
+      height:35px ;
+      padding:0 15px;
+  }
+
+  > textarea{
+      min-width:400px;
+      max-width:400px;
+      height:150px;
+      max-height: 500px;
+
+      padding:10px 15px;
+  }
+
+  > div input{
+      width:350px;
+      height:35px ;
+      padding:0 15px;
+  }
+
+  > div button{
+      height: 35px;
+      padding:0 10px;
+      margin-left:15px;
+  }
 `;
 
 export const IntegrantesBox = styled.div`
+   width:350px;
+   max-height:300px;
+   overflow-y:auto;
+   overflow-x:hidden;
 
+   max-width:450px;
 `;
 
 export const Integrante = styled.div`
+    display:flex;
+    align-items:center;
 
+    margin:5px 0;
+
+   > img{
+       height: 35px;
+       width: 35px;
+       border-radius:50%;
+
+       margin-right:10px;
+   }
 `;
