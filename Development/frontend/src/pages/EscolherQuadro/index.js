0be485140f68ee0c309a40a50d3a3a8c835b19f1
unknown <gustta.apolonio@gmail.com>
2020-11-04 11:25:17 -0300

    Escolher quadro -- done

--- Development/frontend/src/pages/EscolherQuadro/index.js
+++ Development/frontend/src/pages/EscolherQuadro/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import CabecalhoSimples from '../../components/CabecalhoSimples';
 import QuadroButton from '../../components/QuadroButton';
