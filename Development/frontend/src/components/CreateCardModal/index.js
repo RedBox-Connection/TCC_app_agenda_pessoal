52284e948ac20a39bf384e844e938ebf46da3a7d
uArthurSPereira <arthursp7007@gmail.com>
2020-11-10 12:44:19 -0200

    Subindo CreateCard -- Necessario fazer alterações

--- Development/frontend/src/components/CreateCardModal/index.js
+++ Development/frontend/src/components/CreateCardModal/index.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ModalContent, Container, InputTitle, InputWrapper, InputDescription, End } from './styles';
 
-const Modal = ({id = 'Modal', onClose = () => {}}) => {
+import { toast, ToastContainer } from 'react-toastify';
+
+import ApiCards from '../../services/Cards/services';
+import { useHistory } from 'react-router-dom';
+
+const api = new ApiCards();
+
+const Modal = ({id = 'Modal', onClose = () => {}}, props) => {
+
+    //const [loading, setLoading] = false;
+
+    const idQuadro = props.idQuadro;
+    const [nomeCartao, setNomeCartao] = useState('');
+    const [hora, setHora] = useState('');
+    const [data, setData] = useState(''); 
+    const [descricao, setDescricao] = useState('');
+    const [cor, setCor] = useState();
+    const req = {
+        nomeCartao,
+        hora,
+        data,
+        descricao,
+        cor,
+        idQuadro
+    }
+
+    const navegation = useHistory();
+
+    const adicionarCardClick = async () =>{
+        try {
+            //setLoading(true);
+
+            const resp = await api.cadastrarCartaoTarefa(req)
+
+            navegation.push({
+                pathname:"/Meus-quadros"
+            })
+
+            //setLoading(false);
+
+            return resp;
+        } catch (e) {
+            //setLoading(false);
+            toast.error(e.response.data.erro);
+        }
+    }
+
 
     const handleOutsideClick = (e) => {
         if(e.target.id === id) onClose()
@@ -14,23 +60,23 @@ const Modal = ({id = 'Modal', onClose = () => {}}) => {
                 <Container color="white">
 
                     <h3>Criar Tarefa</h3>
-                    
+
                     <InputTitle>
-                        <input type="text" placeholder="Tarefa"/>
+                        <input type="text" placeholder="Tarefa" onChange={e => setNomeCartao(e.target.value)}/>
                     </InputTitle>
 
                     <InputWrapper>
-                        <input type="time"/>
+                        <input type="time"onChange={e => setHora(e.target.value)}/>
 
-                        <input type="date"/>
+                        <input type="date" onChange={e => setData(e.target.value)}/>
                     </InputWrapper>
 
                     <InputDescription>
-                        <textarea placeholder="Descrição"/>
+                        <textarea placeholder="Descrição" onChange={e => setDescricao(e.target.value)}/>
                     </InputDescription>
 
                     <End>
-                        <button id="AddTarefa">Criar Tarefa</button>
+                        <button id="AddTarefa" onClick={adicionarCardClick}>Criar Tarefa</button>
                         <button onClick={onClose} id="Cancel">Cancelar</button>
                     </End>
                 </Container>
